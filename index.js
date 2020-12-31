0da3334dfdb2a1b4b3a6b1faadfc0a793776516f
Antonin Messinger <antonin.messinger@gmail.com>
2020-12-31 14:09:18 +0100

    include index.js in git

---
+++ index.js
@@ -0,0 +1,20 @@
+import { interpret } from 'xstate';
+import machine from './machine.js';
+
+const service = interpret(machine).onTransition(state => {
+  console.log(state.context);
+})
+
+service.start();
+
+service.send('CREATE');
+service.send('SHUFFLE');
+service.send('DEAL');
+service.send('PLAY');
+service.send('PLAY');
+service.send('PLAY');
+service.send('PLAY');
+service.send('PLAY');
+service.send('PLAY');
+service.send('PLAY');
+service.send('PLAY');
