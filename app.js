4cdcbcddc0e4e527a9c6067d7c2baad1ee72f2f7
Antonin Messinger <antonin.messinger@gmail.com>
2021-01-05 18:47:41 +0100

    add webpack/babel/react

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return <h1>Hello</h1>
+}
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.config.cjs
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, 'app.js') },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
