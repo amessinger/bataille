78622a5712ce74e55585aeda77441ad2b9bbabd9
Antonin Messinger <antonin.messinger@gmail.com>
2021-01-06 15:50:29 +0100

    add state machine live visulalization & move Readme to top

--- README.md
+++ README.md
@@ -19,6 +19,14 @@ Run `yarn web` in a terminal in this folder.
 
 The application is served on http://localhost:9000
 
+The interface is quite barebone for now.
+
+![App interface](./docs/app-screenshot.png)
+
+Should you care to allow popups on this page, a new tab allowing you to visualize the state machine *live* will open!
+
+![A live view of the state machine](./docs/state-machine-live-view-screenshot.png)
+
 ## Notes
 
 If you're using [VS Code](https://code.visualstudio.com/) and the [remote container extension](https://code.visualstudio.com/docs/remote/containers), a dev container is available.
--- app/index.js
+++ app/index.js
@@ -1,19 +1,27 @@
 import React, { lazy, Suspense } from 'react';
 import { render } from 'react-dom';
 import { useMachine } from '@xstate/react';
+import { inspect } from "@xstate/inspect";
 import machine from '../machine/index.js';
 import Loading from './components/loading.js';
 import { getStateComponentName } from './utils.js';
 
+inspect({
+  url: "https://statecharts.io/inspect",
+  iframe: false
+});
+
 function App() {
-  const [state, send] = useMachine(machine);
+  const [state, send] = useMachine(machine, { devTools: true });
 
   const Component = lazy(() => import(`./components/states/${getStateComponentName(state)}.js`));
 
   return (
-    <Suspense fallback={<Loading />}>
-      <Component send={send} state={state} />
-    </Suspense>
+    <>
+      <Suspense fallback={<Loading />}>
+        <Component send={send} state={state} />
+      </Suspense>
+    </>
   );
 }
 
